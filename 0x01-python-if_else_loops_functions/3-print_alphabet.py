38d86eec5b109897bdb0dfb4b6f32992571af818
tpauldike <topman4loveworld@gmail.com>
2022-08-25 11:41:50 +0100

    Corrections made

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-for i in range(97, 123):
-    if (i == 101) or (i == 113):
-        continue
-    print(chr(i), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for i in range(97, 123):
     if (i == 101) or (i == 113):
         continue
-    print(chr(i), end="")
+    print("{}".format(chr(i)), end="")
