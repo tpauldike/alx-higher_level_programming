5b10d649b1e4a791839c7b1c770a7c20b2d806a4
tpauldike <topman4loveworld@gmail.com>
2022-08-31 14:59:22 +0000

    applying pycodestyle standard

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{}{}".format((i // 10), (i % 10)), end = ", ")
+    print("{}{}".format((i // 10), (i % 10)), end=", ")
     i += i
-
 print()
