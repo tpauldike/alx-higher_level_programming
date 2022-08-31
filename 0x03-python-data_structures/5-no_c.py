13a3f5b643017b179753aa10fc0188d691ff2eec
tpauldike <topman4loveworld@gmail.com>
2022-08-31 03:43:02 +0000

    task 5 corrected

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_str = ""
     for i in my_string:
-        if i is not 'c' and i is not 'C':
+        if (i is not 'c') and (i is not 'C'):
             new_str += i
     return (new_str)
