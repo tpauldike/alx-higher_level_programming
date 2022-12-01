7d0116e9101a6fee62b4319bce6f8824ff1e46b1
tpauldike <topman4loveworld@gmail.com>
2022-12-01 03:23:32 +0100

    bugs fixed

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-let count = 0;
 
-exports.logMe = function (item) {
-  console.log(count + ': ' + item);
-  count++;
+let counter = 0;
+
+exports.logMe = function count (item) {
+  console.log(`${counter}: ${item}`);
+  counter += 1;
+};
