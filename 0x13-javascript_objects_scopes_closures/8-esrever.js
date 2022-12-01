ddad9faffa518dde34c966bf0a8a4e2808c172f4
tpauldike <topman4loveworld@gmail.com>
2022-12-01 03:13:01 +0100

    Some corrections made

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,7 @@
 // function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-
-  let rlist = [];
+  const rlist = [];
   while (list.length > 0) {
     rlist.push(list.pop());
   }
