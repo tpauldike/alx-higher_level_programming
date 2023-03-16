a7d5d8713cb3406a630ff15dc317b4e4049c24e3
codetrybe <tpauldike@yahoo.com>
2023-03-16 03:05:37 +0100

    advanced tasks solved

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows from hbtn_0d_tvshows_rate by their descending rating of rating.
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
