92fad0678abd945d512356b7eef4cf6a0f22d9d9
tpauldike <topman4loveworld@gmail.com>
2023-04-11 19:18:56 +0000

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if ((process.argv[2] | 0) !== process.argv[2]) {
+if ((process.argv[2] | 0) != process.argv[2]) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + process.argv[2]);
