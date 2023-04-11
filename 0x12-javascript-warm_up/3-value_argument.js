f789e8834a201c635670149e9796c9d2b51bd0de
tpauldike <topman4loveworld@gmail.com>
2023-04-11 18:25:44 +0000

    code modified

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv[2]) {
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else if (process.argv[1]) {
+  console.log('No argument');
 }
