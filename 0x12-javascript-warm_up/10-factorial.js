3fc21f06f53bb13ca5d3903dc9c4bea858991033
tpauldike <topman4loveworld@gmail.com>
2023-04-11 23:59:29 +0000

    file made semistandard compliant

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,5 +2,5 @@
 function factr (n) {
   return n === 0 || isNaN(n) ? 1 : n * factr(n - 1);
 }
-const arg1 = parseInt(process.argv[2])
+const arg1 = parseInt(process.argv[2]);
 console.log(factr(arg1));
