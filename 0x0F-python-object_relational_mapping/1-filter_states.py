b9a6a86a00d75563d9f8be140883107fcfc4f413
tpauldike <topman4loveworld@gmail.com>
2023-04-18 00:42:13 +0000

    select N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+List all states, that has the letter `N`
+as the first letter of its name, from the
+database `hbtn_0e_0_usa`.
+"""
+
+import MySQLdb as db
+from sys import argv
+
+"""
+connect to the database and fetch all data
+from the 'states' table
+"""
+
+if __name__ == '__main__':
+    conn = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
+
+    rows_fetched = db_cursor.fetchall()
+
+    for row in rows_fetched:
+        print(row)
