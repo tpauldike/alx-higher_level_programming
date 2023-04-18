c593232503ff256dc1f92907c2bf5c9a0624308d
tpauldike <topman4loveworld@gmail.com>
2023-04-18 01:35:33 +0000

    error fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         cursor.execute("SELECT cities.id, cities.name, states.name \
                                 FROM cities JOIN states ON cities.state_id \
                                 = states.id ORDER BY cities.id ASC")
-        cities = cursor.fetchall()
+    cities = cursor.fetchall()
 
     if cities is not None:
         for row in cities:
