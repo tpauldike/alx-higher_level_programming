129c7b62f88966d8241e895c152d06be1c896333
tpauldike <topman4loveworld@gmail.com>
2023-04-18 01:33:42 +0000

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A script that lists all cities from the database
+'hbtn_0e_0_usa'
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    connect to the database and execute the querry that
+    will fetch all data from the 'cities' table
+    """
+    conn = db.connect(host="localhost", port=3306,
+                      user=argv[1], passwd=argv[2], db=argv[3])
+
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
+        cities = cursor.fetchall()
+
+    if cities is not None:
+        for row in cities:
+            print(row)
