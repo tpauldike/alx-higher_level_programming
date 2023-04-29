6edaa570200a7a0ff600c5e8aa2d0e12f90fdfc4
tpauldike <topman4loveworld@gmail.com>
2023-04-28 19:54:12 -0700

    I'm a DELETE request

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sI "$1" | grep "Allow" | cut -d " " -f2-
+curl -sX "DELETE" "$1"
